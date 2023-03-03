cf3edd62475a02db466d16c326a6081ce42a610b
Daniel Militão <dgmilitao@gmail.com>
2023-03-03 02:36:52 -0300

    Organiza os exercicios do dia

--- Fundamentos/Bloco-07-higher-order-functions-do-javascript-es6/Dia-01/Exercicio-7.1.js
+++ Fundamentos/Bloco-07-higher-order-functions-do-javascript-es6/Dia-01/Exercicio-7.1.js
@@ -78,8 +78,6 @@ const answerScore = (rightAnswer, studentAnswer) => {
     return -0.5;
   };
 
-
-
 const checkAnswers = (RIGHT_ANSWERS, STUDENT_ANSWERS, action) => {
     let score = 0;
     RIGHT_ANSWERS.forEach((element, index) => {
@@ -91,4 +89,93 @@ const checkAnswers = (RIGHT_ANSWERS, STUDENT_ANSWERS, action) => {
 console.log(checkAnswers(RIGHT_ANSWERS,STUDENT_ANSWERS, answerScore));
 
 
+// 🚀 Organizando uma biblioteca
+// Estes exercícios praticam os conceitos de Higher Order Functions associados a outros temas de fundamentos já vistos,
+//  como arrow functions, template literals e objetos. Essa mistura de conceitos é muito importante para o seu aprendizado, então use tudo o que sabe para resolver os exercícios!
+
+// Utilize o seguinte código como template para realizar os exercícios: 
+
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+// Adicione o código do exercício aqui:
+
+//   Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+
+const authorBornIn1947 = (array) => array.find((array) => array.author.birthYear === 1947).author.name;
  
+console.log(authorBornIn1947(books));
+
+// Retorne o nome do livro com menor número de caracteres (menor nome).
+
+const smallerName = (array) => {
+  let nameBook = '';
+  array.forEach((element) => {
+    if (!nameBook || element.name.length < nameBook.length) {
+        nameBook = element.name;
+    }
+  })
+  return nameBook;
+}
+
+console.log(smallerName(books));

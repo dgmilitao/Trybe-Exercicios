2463082e08bdbb27f010036a8754606ec6ab5e7a
Daniel Militão <dgmilitao@gmail.com>
2023-03-28 15:08:06 -0300

    Organiza os exercicios do dia

---
+++ Front-end/Introducao-ao-React/Dia-01/exercicio-01/src/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+ class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    {'Daniel Militao'}
+                </h1>
+                <p>
+                    Moro em Maceió-AL, tenho 38 anos e sou estudante da Trybe escola de programação.
+                </p>
+                <h2>
+                    Minhas Habilidades
+                </h2>
+                <ul>
+                    <li>Comunicação</li>
+                    <li>Colaborativo</li>
+                    <li>Empatico</li>
+                </ul>
+            </div>
+        );
+    }
+ }
+
+ export default About;
